f158dbea117356bf870ed526f462ce13e11af0cf
Mohamed Mesbah <109096710+mmsbah191@users.noreply.github.com>
2024-11-21 10:44:27 +0200

    Test

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\mmsba\Desktop\CS438\Assignments\Ass3\btes\venv
+set VIRTUAL_ENV=C:\Users\mmsba\Desktop\btes\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
